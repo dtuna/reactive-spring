f8aed6e4a52d89477548d3295c4bf8bbd6ef45a0
dtuna <doga.tuna@gmail.com>
2021-01-19 03:18:58 +0300

    Virtual Time implemented to some of the test cases

--- src/test/java/com/orco/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ src/test/java/com/orco/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 public class FluxAndMonoCombineTest {
     
@@ -23,12 +24,20 @@ public class FluxAndMonoCombineTest {
     
     @Test
     public void combineUsingMerge_WithDelay() {
+        VirtualTimeScheduler.getOrSet();
+        
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
-        Flux<String> flux2 = Flux.just("D", "E", "F");
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
         Flux<String> mergedFlux = Flux.merge(flux1, flux2);
-        StepVerifier.create(mergedFlux.log())
+        // StepVerifier.create(mergedFlux.log())
+        //         .expectSubscription()
+        //         .expectNextCount(6)
+        //         .verifyComplete(); 
+        
+        StepVerifier.withVirtualTime(() -> mergedFlux.log())
                 .expectSubscription()
+                .thenAwait(Duration.ofSeconds(6))
                 .expectNextCount(6)
                 .verifyComplete(); 
     }
