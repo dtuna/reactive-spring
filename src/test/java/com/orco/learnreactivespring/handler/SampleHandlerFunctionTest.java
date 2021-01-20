c4722edadabec7a8f62b441c31ec5962cc222207
dtuna <doga.tuna@gmail.com>
2021-01-20 05:37:16 +0300

    Functional web classes and tests aded

---
+++ src/test/java/com/orco/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,54 @@
+package com.orco.learnreactivespring.handler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SampleHandlerFunctionTest {
+    
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testFlux() {
+        Flux<Integer> streamFlux = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(streamFlux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+    
+    @Test
+    public void testMono() {
+        Integer expectedValue = 1;
+
+        webTestClient.get().uri("/functional//mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(response -> {
+                    assertEquals(expectedValue, response.getResponseBody());
+                });     
+    }
+
+}
